b75262849451bd37737921cb26fed9858f279d34
Austin <pizzahi5@gmail.com>
2023-05-24 11:00:02 -0700

    made_first_rust_project

---
+++ fizzbuzz/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("Hello, fizzbuzz world!");
+
+    //fn fizzbuzz() {
+
+    //}
+}
---
+++ fizzbuzz/target/debug/deps/fizzbuzz-cb3200c3c6809a67.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/PizzaHi5/git_clones/Solana-Encode-Bootcamp/fizzbuzz/target/debug/deps/fizzbuzz-cb3200c3c6809a67.rmeta: src/main.rs
+
+/mnt/c/Users/PizzaHi5/git_clones/Solana-Encode-Bootcamp/fizzbuzz/target/debug/deps/fizzbuzz-cb3200c3c6809a67.d: src/main.rs
+
+src/main.rs:
